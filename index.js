d2a33fcaaf73618e8b41b2c6793e2d81cc03199c
Muhammad Mostafa <mohamedmoustafaa1998@gmail.com>
2025-09-13 22:09:58 +0300

    Enhancemet: Add Global Error Hnadler

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { PrismaClient } = require('./generated/prisma');
+
 const bookRoutes = require('./routes/bookRoutes');
 const borrowerRoutes = require('./routes/borrowerRoutes');
 const borrowRoutes = require('./routes/borrowRoutes');
@@ -7,13 +8,21 @@ const borrowRoutes = require('./routes/borrowRoutes');
 const app = express();
 const prisma = new PrismaClient();
 
+// ---- Body Parser ---- //
 app.use(express.json());
 
+// ---- Routes ---- //
 app.use('/books', bookRoutes);
 app.use('/borrowers', borrowerRoutes);
 app.use('/', borrowRoutes);
 
+// ---- Global Error Handler ---- //
+app.use((err, req, res, next) => {
+  console.error("❌ Unhandled Error:", err);
+  res.status(500).json({ error: "Something went wrong" });
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
