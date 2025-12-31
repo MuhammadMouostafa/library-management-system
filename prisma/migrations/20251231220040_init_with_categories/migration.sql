206d6adcd8eb03ada6669ee3d0ea7e6729cf3484
Muhammad Mostafa <mohamedmoustafaa1998@gmail.com>
2026-01-01 00:02:43 +0200

    Add Category for the schema - reset the database to remove the old data npx prisma migrate reset - apply the change npx prisma migrate dev --name init_with_categories

---
+++ prisma/migrations/20251231220040_init_with_categories/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - Added the required column `categoryId` to the `Book` table without a default value. This is not possible if the table is not empty.
+  - Made the column `imageURL` on table `book` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE `book` ADD COLUMN `categoryId` INTEGER NOT NULL,
+    MODIFY `imageURL` VARCHAR(191) NOT NULL;
+
+-- CreateTable
+CREATE TABLE `Category` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `order` INTEGER NOT NULL DEFAULT 0,
+
+    UNIQUE INDEX `Category_name_key`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Book` ADD CONSTRAINT `Book_categoryId_fkey` FOREIGN KEY (`categoryId`) REFERENCES `Category`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
